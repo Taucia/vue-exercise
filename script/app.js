e5ac2010b3e225415a08f666d52c5ea9a3ee5b0f
taucia <taucia.jackson123@gmail.com>
2022-07-04 14:27:41 +0200

    change

--- script/app.js
+++ script/app.js
@@ -1,9 +1,39 @@
-const app = Vue.createApp ({
-    data: function(){
-        return{
-            name: "Taucia",
-            surname: "Jackson",
-        };
+// const app = Vue.createApp ({
+//     data: function(){
+//         return{
+//             name: "Taucia",
+//             surname: "Jackson",
+//         };
+//     }
+// });
+// app.mount("#app");
+
+
+const app = Vue.createApp({
+    template:` <div v-if='isVisible' id='box1'>
+    <p>{{name}} {{age}}</p>
+    </div>
+    <input type='text' v-model= 'name'/>
+     <input type='text' v-model= 'age'/>
+    <button v-on:click='display'>Click Me</button>`
+    ,
+data:function(){
+    return{
+        name:'taucia',
+        age: 22,
+        isVisible:true
     }
+},
+methods:{
+    display() {
+        if(parseInt(this.age) > 17){
+            this.isVisible = true;
+            console.log('user');
+        }else{
+            this.isVisible = false;
+        }
+        
+    }
+}
 });
-app.mount("#app");
+app.mount('#app');
